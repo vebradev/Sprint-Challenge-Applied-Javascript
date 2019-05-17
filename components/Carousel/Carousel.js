fce37cad1e42749d2701a9c7648d810a3ad15dfd
vebradev <justinas@vebra.dev>
2019-05-17 12:35:38 +0300

    Add Carousel component, works with errors.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,13 +1,45 @@
 class Carousel {
+  constructor(carousel) {
+    this.carousel = carousel;
+    this.left = document.querySelector(".left-button");
+    this.right = document.querySelector(".right-button");
+    this.images = Array.from(document.querySelectorAll(".carousel img"));
+    this.index = 0;
+    this.current = this.images[this.index];
+    this.show(this.current);
+    this.left.addEventListener("click", () => this.previous());
+    this.right.addEventListener("click", () => this.next());
+  }
 
+  show(image) {
+    image.style.display = "block";
+    console.log(this.index);
+  }
+
+  hide(image) {
+    image.style.display = "none";
+  }
+
+  previous() {
+
+  }
+
+  next() {
+    this.hide(this.current);
+    this.index++
+    this.current = this.images[this.index];
+    this.show(this.current);
+  }
+
+  go() {
+    console.log("Go works!");
+  }
 }
-
-let carousel = document.querySelector('.carousel');
-console.log(carousel);
+let carousel = document.querySelector(".carousel");
+const runCarousel = new Carousel(carousel);
+runCarousel.go();
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
-    2. You will need to grab a reference to all of the images
     3. Create a current index
     4. Those buttons are gonna need some click handlers.
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
