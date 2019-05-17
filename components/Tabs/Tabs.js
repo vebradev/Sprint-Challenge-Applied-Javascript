d4884ed1c14a40e069fed5eed776ed0658e326ed
vebradev <justinas@vebra.dev>
2019-05-17 11:01:22 +0300

    Add TabCard component.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -47,22 +47,19 @@ class TabLink {
 class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
-    // this.cardElement;
+    this.cardElement = cardElement;
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
-    // this.cardElement;
+    this.cardElement.style.display = "flex";
   }
-
 }
 
-/* START HERE: 
-
+/* 
+  START HERE: 
 - Select all classes named ".tab" and assign that value to the tabs variable
-
 - With your selection in place, now chain a .forEach() method onto the tabs variable to iterate over the DOM NodeList
-
 - In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a parameter
-
 */
+
 let tabs = document.querySelectorAll('.tab').forEach(tab => new TabLink(tab));
